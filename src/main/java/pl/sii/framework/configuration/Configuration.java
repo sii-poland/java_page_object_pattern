bc07281d0f04b735731e39e770e47ae881d16762
Bogdan Kostka <bkostka@sii.pl>
2019-08-27 17:57:04 +0200

    Fix reading properties from file

--- src/main/java/pl/sii/framework/configuration/Configuration.java
+++ src/main/java/pl/sii/framework/configuration/Configuration.java
@@ -2,17 +2,14 @@ package pl.sii.framework.configuration;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources("classpath:configuration.properties")
 public interface Configuration extends Config {
 
-    @DefaultValue("http://5.196.7.235")
     String applicationAddress();
 
-    @DefaultValue("http://localhost:5566/wd/hub")
     String gridHubUrl();
 
-    @DefaultValue("FIREFOX")
     String browserName();
 
-    @DefaultValue("REMOTE")
     String driverType();
 }
